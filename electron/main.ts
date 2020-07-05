d95b56469c2a1873ddbd28c63945e4e42a41426f
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-07-05 02:30:08 -0300

    feat: add dist script

--- electron/main.ts
+++ electron/main.ts
@@ -9,7 +9,6 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1100,
     height: 700,
-    backgroundColor: '#191622',
     webPreferences: {
       nodeIntegration: true
     }
@@ -20,7 +19,7 @@ function createWindow () {
   } else {
     mainWindow.loadURL(
       url.format({
-        pathname: path.join(__dirname, '../index.html'),
+        pathname: path.join(__dirname, 'renderer/index.html'),
         protocol: 'file:',
         slashes: true
       })
@@ -37,11 +36,11 @@ app.on('ready', createWindow)
   .then(() => {
     if (process.env.NODE_ENV === 'development') {
       installExtension(REACT_DEVELOPER_TOOLS)
-          .then((name) => console.log(`Added Extension:  ${name}`))
-          .catch((err) => console.log('An error occurred: ', err));
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err))
       installExtension(REDUX_DEVTOOLS)
-          .then((name) => console.log(`Added Extension:  ${name}`))
-          .catch((err) => console.log('An error occurred: ', err));
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err))
     }
   })
 app.allowRendererProcessReuse = true
